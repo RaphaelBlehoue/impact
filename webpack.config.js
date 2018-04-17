f6797dcf7884bc53685fc1b5d0f2ee07afb04450
raphael Blehoue <raphaelsampan@gmail.com>
2018-04-17 12:08:53 +0000

    mise a jour template 2

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,12 @@ Encore
     .setPublicPath('/build/front')
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
-    .addEntry('js/main', './assets/front/js/theme/custom.js')
     // css entry
-    .addStyleEntry('css/impact', [
-        './assets/front/css/theme/business.css',
-        './assets/front/css/theme/css/fonts.css',
-        './assets/front/css/theme/css/colors.css',
-        './assets/front/css/theme/onepage/css/et-line.css'
-    ])
     .addStyleEntry('css/app', [
-        './assets/front/style.less',
-        './assets/front/less/dark.less',
-        './assets/front/less/responsive.less',
-        './assets/front/css/swiper.css',
-        './assets/front/css/animate.css',
-        './assets/front/css/magnific-popup.css',
-        './assets/front/css/font-icons.css'
+        './assets/front/css/main.css',
+        './assets/front/css/color.css'
     ])
     //.enableSassLoader()
-    .enableLessLoader()
     // allows legacy applications to use $/jQuery as a global variable
     // .autoProvidejQuery()
     .enableSourceMaps(!Encore.isProduction())
@@ -34,6 +21,7 @@ Encore
         { from: './assets/front/static', to: 'static' }
     ]))
 ;
+
 
 const firstConfig = Encore.getWebpackConfig();
 firstConfig.name = 'firstConfig';
@@ -45,18 +33,7 @@ Encore
     .setPublicPath('/build/back')
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
-    .addStyleEntry('css/core', [
-        './assets/back/vendor/bootstrap/css/bootstrap.min.css',
-        './assets/back/vendor/font-awesome/css/font-awesome.min.css',
-        './assets/back/vendor/themify-icons/css/themify-icons.css',
-        './assets/back/vendor/animsition/css/animsition.min.css',
-        './assets/back/vendor/perfect-scrollbar/css/perfect-scrollbar.min.css'
-    ])
-    .addStyleEntry('css/app', [
-        './assets/back/css/scss/app.scss',
-        './assets/back/css/style.scss'
-    ])
-    .enableSassLoader()
+    .addStyleEntry('css/core', './assets/back/css/core.css')
     .enableSourceMaps(!Encore.isProduction())
     .enableSourceMaps(true)
     .enableVersioning(Encore.isProduction())
